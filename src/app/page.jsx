80521a8c677c8a52ae91dc13db1c559b8d52cdb2
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-10-18 11:58:58 -0300

    um pouco de tailwhind

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,13 +6,6 @@ export default function Home() {
     <>
     <h1>Home!</h1>
     <div>
-      <h2>Meus Produtos</h2>
-      <ul>
-        <li><Link href="/produtos/tenis">Tênis</Link></li>
-        <li><Link href="/produtos/calca">Calca</Link></li>
-        <li><Link href="/produtos/meia">Meia</Link></li>
-        <li><Link href="/produtos/camisa">Camisa</Link></li>
-      </ul>
       <figure>
       <Image src="/img/home.jpg" alt="Placa escura" width={700} height={500}></Image>
       </figure>
