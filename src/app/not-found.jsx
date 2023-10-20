9393bbabb80214bbcc9fcd3a4e83ff4a34f667e4
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-10-20 18:59:44 -0300

    página de erro, estrutura, tailwind

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div>
-        <h1>OOPS! 404!</h1>
-        <p>Página não encontrada!</p>
+    <div className="bg-red-500 font-extrabold fixed w-full h-full">
+        <h1 className="text-center ">OOPS! 404!</h1>
+        <div>
+          <figure>
+            <Image className="block m-auto w-80 rounded-lg" src="https://http.cat/images/404.jpg" alt="Gato no jornal" width={750} height={600} />
+          </figure>
+        </div>
+        <p className="text-center">Página não encontrada!</p>
+
     </div>
   )
 }
---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,13 @@
+
+export default function Rodape() {
+    return (
+      <footer className="rodape">
+          <div>
+              <input type="text" />
+              <p>Desenvolvido por <span>RM98626</span></p>
+              <p>NEXT.js - React.js - tailwind</p>
+          </div>
+      </footer>
+    )
+  }
+  
